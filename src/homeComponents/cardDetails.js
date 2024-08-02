66e6218e158e849e73c864843209b3d076290086
freakinaman <amanpiyushsharma@gmail.com>
2024-08-02 23:36:24 +0530

    Updated files with latest changes

--- src/homeComponents/cardDetails.js
+++ src/homeComponents/cardDetails.js
@@ -16,69 +16,69 @@ export function getDetails(){
            route:"/pathfinder",
            img:graph
        },
+    //    {
+    //        id:2,
+    //        title:"Recursion Tree",
+    //        description:"The process in which a function calls itself directly or indirectly is called recursion. Work in progress",
+    //        route:"/graph",
+    //        img:Recursion
+    //    },
        {
            id:2,
-           title:"Recursion Tree",
-           description:"The process in which a function calls itself directly or indirectly is called recursion. Work in progress",
-           route:"/graph",
-           img:Recursion
-       },
-       {
-           id:3,
            title:"Sorting Algorithm",
            description:"Compare different sorting algorithms",
            route:"/sort",
            img:sort
        },
        {
-           id:4,
+           id:3,
            title:"Recursive Sorting",
            description:"Compare different recursive sorting algorithms",
            route:"/recursivesort",
            img:sort
        },
        {
-           id:5,
+           id:4,
            title:"N Queen",
            description:"The N queens puzzle is the problem of placing N chess queens on an N*N chessboard so that no two queens threaten each other",
            route:"/nqueen",
            img:queen
        },
+    //    {
+    //        id:6,
+    //        title:"Turing Machine",
+    //        description:"A Turing machine is a mathematical model of computation that defines an abstract machine that manipulates symbols on a strip of tape according to a table of rules",
+    //        route:"/turing",
+    //        img:turing
+    //    },
        {
-           id:6,
-           title:"Turing Machine",
-           description:"A Turing machine is a mathematical model of computation that defines an abstract machine that manipulates symbols on a strip of tape according to a table of rules",
-           route:"/turing",
-           img:turing
-       },
-       {
-           id:7,
+           id:5,
            title:"Prime Numbers",
            description:"Visualize how Seive is better than brute force",
            route:"/prime",
            img:primes
        },
        {
-           id:8,
+           id:6,
            title:"Convex Hull",
            description:"The convex hull of a set of points is the smallest convex polygon that contains all the points of it",
            route:"/convexhull",
            img:convex
        },
        {
-           id:9,
+           id:7,
            title:"Binary Search",
            description:"Binary search is an efficient algorithm for finding an item from a sorted list of item",
            route:"/binarysearch",
            img:binSearch
        },
-       {
-           id:10,
-           title:"15 Puzzle",
-           description:"The 15 puzzle is a sliding puzzle having 15 square tiles numbered 1–15 in a frame that is 4 tiles high and 4 tiles wide, leaving one unoccupied tile position",
-           route:"/15puzzle",
-           img:puzzle
-       }
+    //    {
+    //        id:10,
+    //        title:"15 Puzzle",
+    //        description:"The 15 puzzle is a sliding puzzle having 15 square tiles numbered 1–15 in a frame that is 4 tiles high and 4 tiles wide, leaving one unoccupied tile position",
+    //        route:"/15puzzle",
+    //        img:puzzle
+    //    }
 
    ]
 }
--- src/homeComponents/footer.jsx
+++ src/homeComponents/footer.jsx
@@ -7,16 +7,16 @@ class Footer extends Component {
             <footer class="page-footer font-small special-color-dark pt-4 ">
 
                 <div className='flex-wrap' style={{textAlign:"center"}}>
-                    <div className='m-2'><GitHubButton href="https://github.com/TamimEhsan" data-show-count="true" aria-label="Follow @TamimEhsan on GitHub">Follow @TamimEhsan</GitHubButton></div>
-                    <GitHubButton href="https://github.com/TamimEhsan/Pathfinder-2.0" data-icon="octicon-star" data-show-count="true" aria-label="Star TamimEhsan/Pathfinder-2.0 on GitHub">Star</GitHubButton>
+                    <div className='m-2'><GitHubButton href="https://github.com/freakinaman" data-show-count="true" aria-label="Follow @freakinaman on GitHub">Follow @freakinaman</GitHubButton></div>
+                    {/* <GitHubButton href="https://github.com/freakinaman" data-icon="octicon-star" data-show-count="true" aria-label="Star freakinaman on GitHub">Star</GitHubButton>
                     &nbsp
-                    <GitHubButton href="https://github.com/TamimEhsan/Pathfinder-2.0/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork TamimEhsan/Pathfinder-2.0 on GitHub">Fork</GitHubButton>
-                    &nbsp
-                    <img src="https://visitor-badge.laobi.icu/badge?page_id=TamimEhsan.AlgorithmVisualizer" alt="Hits"/>
+                    <GitHubButton href="https://github.com/freakinaman" data-icon="octicon-repo-forked" data-show-count="true" aria-label="freakinaman">Fork</GitHubButton>
+                    &nbsp */}
+                    {/* <img src="https://visitor-badge.laobi.icu/badge?page_id=freakinaman" alt="Hits"/> */}
                 </div>
 
-                <div class="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://github.com/tamimehsan" style={{color: "#65b6f0"}}> Tamim Ehsan</a>
+                <div class="footer-copyright text-center py-3"> 2024 Copyright:
+                    <a href="https://github.com/freakinaman" style={{color: "#65b6f0"}}> Aman Sharma</a>
                 </div>
 
             </footer>
